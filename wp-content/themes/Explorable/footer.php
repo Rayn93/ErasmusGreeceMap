a468fa25b42ecd75ade57618eb92e5c3998d9d78
Rob <robert.saternus@gmail.com>
2017-10-26 17:30:31 +0300

    Styling + change footer credentials

--- wp-content/themes/Explorable/footer.php
+++ wp-content/themes/Explorable/footer.php
@@ -3,7 +3,7 @@
 		<div class="container">
 			<?php get_sidebar( 'footer' ); ?>
 
-			<p id="copyright"><?php printf( __( 'Designed by %1$s | Powered by %2$s', 'Explorable' ), '<a href="http://www.elegantthemes.com" title="Premium WordPress Themes">Elegant Themes</a>', '<a href="http://www.wordpress.org">WordPress</a>' ); ?></p>
+			<p id="copyright"><?php printf( __( 'Created by %1$s', 'Explorable' ), '<a href="http://robertsaternus.pl" title="The Best Web Developer">Robert Saternus</a>' ); ?></p>
 		</div> <!-- end .container -->
 	</footer> <!-- end #main-footer -->
 <?php endif; ?>
